80d6cf23b14f57ebebfad1a0e4cf2826e5075f09
James Heald <jh97uk@icloud.com>
2020-08-19 17:37:12 -0700

    Added departure update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const Schema = require('./ValidationSchema.js');
 const Database = require('./DatabaseSchema.js');
 const { Sequelize } = require('sequelize');
+const { date } = require('@hapi/joi');
 
 const port = 4000;
 
@@ -12,18 +13,36 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
-
-
 app.post ('/customer/entry', function(request, response){
     var validation = Schema.phoneNumber.validate(request.body);
     if(validation.error){
         throw new Error(validation.error.message);
     }
-    Database.Tables.Customers.create({
-        phoneNumber:request.body.number,
-        entryTimestamp: new Date().getTime()
+
+    Database.Tables.Customers.count({
+        where:{
+            phoneNumber: request.body.number,
+            departureTimestamp: null
+        }
+    }).then(function(customerWithNumberCount){
+        if(customerWithNumberCount == 0){
+            Database.Tables.Customers.create({
+                phoneNumber:request.body.number,
+                entryTimestamp: new Date().getTime()
+            }).then(function(test){
+                response.send({message:'success'});
+            })
+        } else{
+            Database.Tables.Customers.update({departureTimestamp:new Date().getTime()}, {
+                where:{
+                    phoneNumber:request.body.number,
+                    departureTimestamp:null
+                }
+            }).then(function(){
+                response.send({message:'success'});
+            })
+        }
     })
-    response.send({message:'success'});
 });
 
 app.use(function(error, request, response, next){
