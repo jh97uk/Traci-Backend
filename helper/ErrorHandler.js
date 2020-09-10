98fd85acba6491721d70f67829f9278d092351ba
James Heald <jh97uk@icloud.com>
2020-09-10 20:33:08 +0100

    Changed the error objects returned to be more clear

--- helper/ErrorHandler.js
+++ helper/ErrorHandler.js
@@ -2,8 +2,8 @@ module.exports = errorHandler;
 
 function errorHandler(error, request, response, next){
     if(error.name === 'UnauthorizedError'){
-        return response.status(401).json({message:'Invalid token!'});
+        return response.status(401).json({error:'Invalid token!'});
     } else{
-        return response.status(500).json({message:error.message});
+        return response.status(500).json({error:error.message});
     }
 }
