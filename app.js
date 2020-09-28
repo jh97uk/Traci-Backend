b756bd23484c456c48b8fbbc1ee0348c11d884f9
jh97uk <jh97uk@icloud.com>
2020-09-28 15:00:06 +0100

    Added check for routes to see if Traci is setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const jwt = require('./helper/jwt.js');
 const errorHandler = require('./helper/ErrorHandler.js');
+const editJsonFile = require("edit-json-file");
 
 const port = 4000;
 
@@ -13,6 +14,18 @@ app.use(express.json());
 app.use(cors());
 app.use(jwt());
 
+app.use(function(request, response, next){
+    const config = editJsonFile(`${__dirname}/config.json`).toObject();
+    if(!config.dashboardPassword){
+        if(request.url == "/users")
+            next();
+        else
+            response.status(500).send({message:"Admin setup required!"})
+    } else{
+        next();
+    }
+})
+
 app.use('/users', require('./service/User.js'));
 app.use('/customer', require('./service/Customer.js'))
 app.use(errorHandler);
@@ -20,8 +33,7 @@ app.use(errorHandler);
 app.use(function(error, request, response, next){
     response.status(500).send({message:error.message});
 })
+
 app.listen(port, function(){
     console.log("Listening!");
 })
-
-
