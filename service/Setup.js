74e3ad209b5a0f54fe198b20b916713b123382bb
James <jh97uk@icloud.com>
2020-10-02 20:50:04 +0100

    Added routes for adding establishment info

--- service/Setup.js
+++ service/Setup.js
@@ -2,10 +2,22 @@ const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 const Joi = require('@hapi/joi')
-
+const Schema = require('../ValidationSchema.js');
 const editJsonFile = require("edit-json-file");
 
 class Setup{  
+
+    static setupEstablishment(request, response, next){
+        var validation = Schema.Establishment.validate(request.body);
+        if(validation.error)
+            throw new Error(validation.error.message);
+        let configJsonFile = editJsonFile(`${__dirname}/../config.json`);
+        configJsonFile.set('establishmentName', request.body.establishmentName);
+        configJsonFile.set('establishmentMessage', request.body.establishmentMessage);
+        configJsonFile.save();
+        response.send({status:'success'});
+    }
+
     static checkPasswordSetStatus(request, response, next){
         let configJsonFile = editJsonFile(`${__dirname}/../config.json`);
         const password = configJsonFile.get('dashboardPassword');
@@ -18,4 +30,6 @@ class Setup{
 }
 
 router.get('/', Setup.checkPasswordSetStatus);
+router.post('/establishment', Setup.setupEstablishment)
+router.patch('/establishment', Setup.setupEstablishment);
 module.exports = router;
